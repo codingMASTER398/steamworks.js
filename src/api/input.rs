71ce8c2aed03186161d21ea4ff351a20c8c10645
coding398 <55938439+codingMASTER398@users.noreply.github.com>
2024-09-18 15:01:12 +1000

    Update input.rs

--- src/api/input.rs
+++ src/api/input.rs
@@ -199,7 +199,8 @@ pub mod input {
     #[napi]
     pub fn get_glyph_for_action_origin(origin: BigInt) -> String {
         let client = crate::client::get_client();
-        let origin_value = origin.get_u64().1 as steamworks_sys::EInputActionOrigin; // Convert BigInt to EInputActionOrigin
-        client.input().get_glyph_for_action_origin(origin_value)
+        let origin_value = origin.get_u64().1 as u64; // Convert BigInt to u64
+        let origin_enum = unsafe { std::mem::transmute::<u64, steamworks_sys::EInputActionOrigin>(origin_value) }; // Convert u64 to EInputActionOrigin
+        client.input().get_glyph_for_action_origin(origin_enum)
     }
 }
